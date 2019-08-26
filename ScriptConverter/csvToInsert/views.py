bd023562c2bd858fcfe14f32f2c61e27bf75e8ae
varunkapoor1994 <49808998+varunkapoor1994@users.noreply.github.com>
2019-08-26 21:14:09 +0530

    updated

--- ScriptConverter/csvToInsert/views.py
+++ ScriptConverter/csvToInsert/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse 
+from django.http import HttpResponse,JsonResponse
+from . import service
 
 
 def getheaders(request):
-    #csv_file=request.POST.get("csvFile")
-    csv_file = request.FILES.get('csvFile')
-    #print(file_data)
-    print(type(csv_file))
-    file_data = csv_file.read().decode("utf-8")
-    lines = file_data.split("\n")
-    return JsonResponse(lines[0],safe=False)
+        if request.method == 'POST':
+            response={}
+            try:
+                if  not request.FILES.get('csvFile'):
+                    response['error']='Please select a file'
+                    return JsonResponse(response)
+
+                if not request.FILES.get('csvFile').name.endswith('.csv'):
+                    response['error']='Please select a csv file'
+                    return JsonResponse(response)
+
+                else:
+                    csv_file = request.FILES.get('csvFile')
+                    file_data = csv_file.read().decode("utf-8")
+                    headers=service.get_headers(file_data)
+                    response['headers']=headers
+                    return JsonResponse(response)
+
+            except Exception as e:
+                response['error']=str(e)
+                return JsonResponse(response)
 
 
 def index(request):
