1f41c86f7a4ae1334cbe928bfd1e0b841be8e364
varunkapoor1994 <49808998+varunkapoor1994@users.noreply.github.com>
2019-08-27 21:57:30 +0530

    v

--- ScriptConverter/csvToInsert/urls.py
+++ ScriptConverter/csvToInsert/urls.py
@@ -1,7 +1,10 @@
 from . import views
 from django.urls import path
 
+
+app_name = 'csv-to-insert'
 urlpatterns = [
     path('getheaders', views.getheaders, name='get_headers'),
+    path('createscript', views.create_script, name='createScript'),
     path('', views.index, name='index')
 ]
--- ScriptConverter/csvToInsert/views.py
+++ ScriptConverter/csvToInsert/views.py
@@ -29,3 +29,28 @@ def getheaders(request):
 
 def index(request):
     return render(request,'csvToInsert/index.html')
+
+
+def create_script(request):
+    if request.method=='POST':
+        try:
+            if  not request.FILES.get('csvfile'):
+                message="Please uplaod a file"
+
+            if not request.FILES.get('csvfile').name.endswith('.csv'):
+                message="Please select a csv file"
+
+            if not request.POST.get("table"):
+                message="Please enter a table name"
+
+            if not request.POST.get("outputFileName"):
+                outputfile=request.FILES.get('csvfile').name.split('.')[0]+".sql"
+
+            if request.POST.get("replaceheaders"):
+                replace=request.POST.get("replacecolumn")
+                replacewith=request.POST.get("repacecolumnwith")
+
+            return HttpResponse("Creating form")
+        except Exception as e:
+            print(e)
+        return HttpResponse("Creating form")
